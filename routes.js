e90656ce7baef165d0d11dd1330ff52bee3d56f9
Tiago Bittencourt <tiago.sbittencourt@icloud.com>
2023-06-05 10:31:44 -0300

    Criando middleware loginRequired

--- routes.js
+++ routes.js
@@ -4,6 +4,8 @@ const homeController = require('./src/controllers/homeController')
 const loginController = require('./src/controllers/loginController')
 const contatoController = require('./src/controllers/contatoController')
 
+const { loginRequired } = require('./src/middlewares/middleware')
+
 // Rotas da home
 route.get('/', homeController.index)
 
@@ -14,5 +16,5 @@ route.post('/login/login', loginController.login)
 route.get('/login/logout', loginController.logout)
 
 // Rotas de contato
-route.get('/contato/index', contatoController.index)
+route.get('/contato/index', loginRequired, contatoController.index)
 module.exports = route
--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -1,3 +1,3 @@
 exports.index = (req, res) => {
-    res.send('oi')
+    res.send('contato')
 }
